7511126eae72c679e6a134f7638b7c7e8199b965
marc thomas <marc.thomas5608@gmail.com>
2026-04-02 14:54:55 +0200

    fichier seed avec projet cree

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+puts "initialisation..."
 Project.destroy_all
-
-Project.create(name: "REPLAY", content: "backOffice intelligent pour la revalorisation de jouets de seconde main")
+puts "ensemble des seeds detruites"
+Project.create(name: "REPLAY", content: "backOffice intelligent pour la revalorisation de jouets de seconde main", started_at: "2026-03-25", link: "https://re-playcopy-533712de4e5a.herokuapp.com/")
+puts "ensemble des nouvelles seeds crees"
