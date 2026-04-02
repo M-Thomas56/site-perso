37f2f4b88040f01ea2e4044f7f405e04e0f5c7aa
marc thomas <marc.thomas5608@gmail.com>
2026-04-02 16:03:04 +0200

    mise en place des routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "projects", to: "projects#index"
   get "about", to: "pages#about"
-  get "contacts", to: "pages#about"
+  get "contacts", to: "pages#contacts"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
